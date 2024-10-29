29087a838a099198631069f3e0d387f9aa86a84d
anupranaythouta <anupranaythouta2005@gmail.com>
2024-10-29 18:24:24 -0500

    Cpp files commit

---
+++ Direction.h
@@ -0,0 +1,17 @@
+//
+// Created by thoutaa on 10/29/2024.
+//
+
+#ifndef DIRECTION_H
+#define DIRECTION_H
+
+
+
+enum class Direction {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST
+};
+
+#endif //DIRECTION_H
