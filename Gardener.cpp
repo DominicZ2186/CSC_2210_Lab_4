3ff7177c8c057ba41dad9be5d80deb995a0ccbda
anupranaythouta <anupranaythouta2005@gmail.com>
2024-11-09 18:33:15 -0600

    Gardner implementations

--- Gardener.cpp
+++ Gardener.cpp
@@ -15,16 +15,26 @@ Gardener::Gardener(Room* startRoom) {
 }
 
 void Gardener::move(Direction direction) {
+    //have the logic to move the gardener based on direction.
 
 }
 
-void Gardener::useWeapon(size_t weaponIndex, Direction direction) {
-
+void Gardener::useWeapon(size_t index, Direction dir) {
+    if (index < inventory.size() && inventory[index] != nullptr) {
+        inventory[index]->use(dir, nullptr);  //pass the game instance
+    } else {
+        std::cout << "Invalid weapon selection!\n";
+    }
 }
 
 
-void Gardener::pickUpWeapon(Weapon *) {
 
+void Gardener::pickUpWeapon(Weapon *weapon) {
+    if (weapon != nullptr) {
+        inventory.push_back(weapon);
+        weapon->pickUp();
+        std::cout << "Picked up a " << (dynamic_cast<Spray*>(weapon) ? "Spray" : "Garden Shears") << "!\n";
+    }
 }
 
 void Gardener::displayInverntory() const {
