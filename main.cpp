fee618ad70aed618cd9d550721ea5dcdeb851605
DominicZ2186 <zagrad@msoe.edu>
2024-11-10 18:13:05 -0600

    Added ability to choose between debug mode or not

--- main.cpp
+++ main.cpp
@@ -5,7 +5,24 @@ using namespace std;
 
 
 int main() {
-    Game* game = new Game(true);
+    char input;
+    cout << "Would you like to play in debug mode? (Y)es or (N)o" << endl;
+    do {
+        cin >> input;
+        if (input != 'y' && input != 'n') {
+            cout << "Invalid input, please enter 'y' for yes or 'n' for no." << endl;
+            cin.clear();
+            cin.ignore();
+        }
+    } while (input != 'y' && input != 'n');
+    input = tolower(input);
+    bool debug;
+    if (input == 'y') {
+        debug = true;
+    } else {
+        debug = false;
+    }
+    Game* game = new Game(debug);
     game->play();
     delete game;
     return 0;
